dfd31235701d365a286191677ad3b283a1d766fe
4479 <142999203+4479cantcode@users.noreply.github.com>
2026-03-27 06:09:10 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,6 @@
 local gameid = game.PlaceId
 
-if gameid == 9674224595 then
+if game.GameId == 9674224595 or game.PlaceId == 77393318863643 then
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/03511297ef1750389373dda4e13652c8305d1fddf86fe1304d6fff3ff2ec7aaa/download"))()
 else
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/61f9e4e53e0d639db1122d02cc47aec30905c362808b723ee3bddbaad6721c9c/download"))()
